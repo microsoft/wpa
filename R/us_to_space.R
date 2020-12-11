1126bd8abf582babe211fe41f6aa21d5a189a451
Martin Chan <martinchan53@gmail.com>
2020-12-11 13:17:42 +0000

    chore: one function per file
    Move maxmin() and us_to_space() to their own R files instead of under keymetrics_scan()

---
+++ R/us_to_space.R
@@ -0,0 +1,13 @@
+#' @title Replace underscore with space
+#'
+#' @description Convenience function to convert underscores to space
+#'
+#' @param x String to replace all occurrences of `_` with a single space
+#'
+#' @examples
+#' us_to_space("Meeting_hours_with_manager_1_on_1")
+#'
+#' @export
+us_to_space <- function(x){
+  gsub(pattern = "_", replacement = " ", x = x)
+}
