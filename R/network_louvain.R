3b5d858c1cd5609a2bab279d22692d8a73527556
Martin Chan <martinchan53@gmail.com>
2020-12-18 17:46:45 +0000

    feat: network_louvain
    Improve formatting, return options, and documentation

--- R/network_louvain.R
+++ R/network_louvain.R
@@ -11,13 +11,30 @@
 #'
 #' @param data Data frame containing a Person to Person query.
 #' @param hrvar String containing the HR attribute to be matched in the dataset.
-#' @param return String specifying what output to return.
+#' @param bg_fill String to specify background fill colour.
+#' @param font_col String to specify font and link colour.
+#' @param node_alpha A numeric value between 0 and 1 to specify the transparency of the nodes.
+#' @param algorithm String to specify the node placement algorithm to be used. Defaults to "fr" for the force-directed
+#' algorithm of Fruchterman and Reingold. See <https://rdrr.io/cran/ggraph/man/layout_tbl_graph_igraph.html> for a
+#' full list of options.
+#' @param return String specifying what output to return.Valid return options include:
+#'   - 'plot-louvain': return a network plot coloured by louvain communities.
+#'   - 'plot-hrvar': return a network plot coloured by HR attribute.
+#'   - 'plot-sankey': return a sankey plot combining communities and HR attribute.
+#'   - 'table': return a vertex summary table with counts in communities and HR attribute.
+#'   - 'data': return a vertex data file that matches vertices with communities and HR attributes.
 #'
 #' @import ggraph
 #' @import dplyr
 #'
 #' @export
-network_louvain <- function(data, hrvar, return){
+network_louvain <- function(data,
+                            hrvar,
+                            bg_fill = "#000000",
+                            font_col = "#FFFFFF",
+                            node_alpha = 0.8,
+                            algorithm = "mds",
+                            return){
 
   ## Set variables
   TO_hrvar <- paste0("TieOrigin_", hrvar)
@@ -60,38 +77,43 @@ network_louvain <- function(data, hrvar, return){
   g <-
     g_raw %>%
     # Add louvain partitions to graph object
-    igraph::set_vertex_attr("cluster", value = igraph::membership(lc)) # Return membership - diff from Leiden
+    igraph::set_vertex_attr("cluster", value = as.character(igraph::membership(lc))) %>% # Return membership - diff from Leiden
+    igraph::simplify()
 
   ## Create vertex table
   vertex_tb <-
     g %>%
     igraph::get.vertex.attribute() %>%
-    as_tibble() %>%
-    count(!!sym(hrvar), cluster)
+    as_tibble()
+
+  g_layout <-
+    g %>%
+    ggraph::ggraph(layout = "igraph", algorithm = algorithm)
 
   ## Return
   if(return == "plot-louvain"){
-    g %>%
-      ggraph::ggraph(layout = "stress") +
-      ggraph::geom_edge_link(colour = "lightgrey", edge_width = 0.1) +
-      ggraph::geom_node_point(aes(colour = cluster), alpha = 0.8) +
+    g_layout +
+      ggraph::geom_edge_link(colour = "lightgrey", edge_width = 0.01, alpha = 0.15) +
+      ggraph::geom_node_point(aes(colour = cluster), alpha = node_alpha) +
       theme_void() +
       theme(legend.position = "bottom",
-            # legend.background = element_rect(fill = "black"),
-            # plot.background = element_rect(fill = "black"),
-            # text = element_text(colour = "white"),
+            legend.background = element_rect(fill = bg_fill),
+            plot.background = element_rect(fill = bg_fill),
+            text = element_text(colour = font_col),
             axis.line = element_blank()) +
       labs(title = "Person to person collaboration with Community Detection",
-           subtitle = "Based on louvain algorithm and Strong Tie Score",
+           subtitle = "Based on Louvain algorithm and Strong Tie Score",
            y = "",
            x = "")
   } else if(return == "plot-hrvar"){
-    g %>%
-      ggraph::ggraph(layout = "stress") +
-      ggraph::geom_edge_link(colour = "lightgrey", edge_width = 0.1) +
-      ggraph::geom_node_point(aes(colour = !!sym(hrvar)), alpha = 0.8) +
+    g_layout +
+      ggraph::geom_edge_link(colour = "lightgrey", edge_width = 0.01, alpha = 0.15) +
+      ggraph::geom_node_point(aes(colour = !!sym(hrvar)), alpha = node_alpha) +
       theme_void() +
       theme(legend.position = "bottom",
+            legend.background = element_rect(fill = bg_fill),
+            plot.background = element_rect(fill = bg_fill),
+            text = element_text(colour = font_col),
             axis.line = element_blank()) +
       labs(title = "Person to person collaboration",
            subtitle = paste0("Showing ", hrvar),
@@ -99,6 +121,11 @@ network_louvain <- function(data, hrvar, return){
            x = "")
 
   } else if(return == "table"){
+
+    vertex_tb %>%
+      count(!!sym(hrvar), cluster)
+
+  } else if(return == "data"){
 
     vertex_tb
 
