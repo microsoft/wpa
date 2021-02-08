257cce7d0620c7bd968166713a3dbcf0836bac10
Martin Chan <martinchan53@gmail.com>
2021-02-08 22:03:30 +0000

    fix: time formats
    An error gets thrown up when the working start/end time is in the format of "7:30", rather than "07:30". This is overly strict and is now rectified.

--- R/flag_outlooktime.R
+++ R/flag_outlooktime.R
@@ -37,9 +37,39 @@ flag_outlooktime <- function(data, threshold = c(4, 15), return = "message"){
   #
   # pad_times <- Vectorize(pad_times)
 
-  if(any(nchar(data$WorkingStartTimeSetInOutlook) != 5 | nchar(data$WorkingEndTimeSetInOutlook) != 5)){
+  ## Clean `WorkingStartTimeSetInOutlook`
+
+  if(any(grepl(pattern = "\\d{1}:\\d{1,2}", x = data$WorkingStartTimeSetInOutlook))){
+
+    # Pad two zeros and keep last five characters
+    data$WorkingStartTimeSetInOutlook <-
+      paste0("00", data$WorkingStartTimeSetInOutlook) %>%
+      substr(start = nchar(.) - 4, stop = nchar(.))
+
+  }
+
+  ## Clean `WorkingEndTimeSetInOutlook`
+
+  if(any(grepl(pattern = "\\d{1}:\\d{1,2}", x = data$WorkingEndTimeSetInOutlook))){
+
+    # Pad two zeros and keep last five characters
+    data$WorkingEndTimeSetInOutlook <-
+      paste0("00", data$WorkingEndTimeSetInOutlook) %>%
+      substr(start = nchar(.) - 4, stop = nchar(.))
+
+
+  }
+
+  if(
+      any(
+        !grepl(pattern = "\\d{1,2}:\\d{1,2}", x = data$WorkingStartTimeSetInOutlook) |
+        !grepl(pattern = "\\d{1,2}:\\d{1,2}", x = data$WorkingEndTimeSetInOutlook)
+      )
+    ){
+
     stop("Please check data format for `WorkingStartTimeSetInOutlook` or `WorkingEndTimeSetInOutlook.\n
          These variables must be character vectors, and have the format `%H:%M`, such as `07:30` or `23:00`.")
+
   }
 
   clean_times <- function(x){
