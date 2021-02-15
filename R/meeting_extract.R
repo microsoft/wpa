58b2205220eaf6a3976d0d711eb8cccb7f5432de
Martin Chan <martinchan53@gmail.com>
2021-02-15 23:46:04 +0000

    feat: update mt_data for meeting_extract()

--- R/meeting_extract.R
+++ R/meeting_extract.R
@@ -33,6 +33,13 @@
 #'   top `n` low engagement meetings
 #'
 #' @import dplyr
+#' @importFrom data.table ":=" "%like%" "%between%"
+#'
+#' @examples
+#' meeting_extract(mt_data,
+#'                 recurring_only = FALSE,
+#'                 top_n = 10,
+#'                 return = "table")
 #'
 #' @export
 meeting_extract <- function(data,
@@ -44,35 +51,44 @@ meeting_extract <- function(data,
 
   if(recurring_only == TRUE){
 
-    mq_df <- filter(mq_df, IsRecurring == TRUE)
+    data <- filter(data, IsRecurring == TRUE)
 
   }
 
-  mq_df <-
+  mq_dt <-
     data %>%
     mutate(Emails_sent_per_attendee_hour =
              Emails_sent_during_meetings / Attendee_meeting_hours) %>%
     mutate(AttendanceRate = Attendees / Invitees) %>%
-    summarise(
-      Sum_Attendee_meeting_hours = sum(Attendee_meeting_hours),
-      Mean_Attendee_meeting_hours = mean(Attendee_meeting_hours),
-      Sum_Attendees = sum(Attendees),
-      Mean_Attendees = mean(Attendees),
-      Sum_Emails_sent_during_meetings = sum(Emails_sent_during_meetings),
-      Mean_Emails_sent_during_meetings = mean(Emails_sent_during_meetings),
-      Sum_Emails_sent_per_attendee_hour = sum(Emails_sent_per_attendee_hour),
-      Mean_Emails_sent_per_attendee_hour = mean(Emails_sent_per_attendee_hour),
-      Sum_DurationHours = sum(DurationHours),
-      Mean_DurationHours = mean(DurationHours),
-      Sum_Total_meeting_cost = sum(Total_meeting_cost),
-      Mean_Total_meeting_cost = mean(Total_meeting_cost),
-      Mean_AttendanceRate = mean(AttendanceRate),
-      Mean_Invitees = mean(Invitees),
-      NRecurring = n()
-    ) %>%
-    filter(Mean_Emails_sent_per_attendee_hour >= 2) %>%
-    arrange(desc(Sum_Attendee_meeting_hours)) %>%
-    slice(1:top_n)
+    data.table::as.data.table()
+
+
+  mq_df <-
+    mq_dt %>%
+    .[,
+      .(
+        Subject = first(Subject), # First instance
+        Sum_Attendee_meeting_hours = sum(Attendee_meeting_hours),
+        Mean_Attendee_meeting_hours = mean(Attendee_meeting_hours),
+        Sum_Attendees = sum(Attendees),
+        Mean_Attendees = mean(Attendees),
+        Sum_Emails_sent_during_meetings = sum(Emails_sent_during_meetings),
+        Mean_Emails_sent_during_meetings = mean(Emails_sent_during_meetings),
+        Sum_Emails_sent_per_attendee_hour = sum(Emails_sent_per_attendee_hour),
+        Mean_Emails_sent_per_attendee_hour = mean(Emails_sent_per_attendee_hour),
+        Sum_DurationHours = sum(DurationHours),
+        Mean_DurationHours = mean(DurationHours),
+        Sum_Total_meeting_cost = sum(Total_meeting_cost),
+        Mean_Total_meeting_cost = mean(Total_meeting_cost),
+        Mean_AttendanceRate = mean(AttendanceRate),
+        Mean_Invitees = mean(Invitees),
+        NRecurring = .N # Number of recurrences
+      ),
+      by = MeetingId] %>%
+    .[Mean_Emails_sent_per_attendee_hour >= 2] %>%
+    .[order(-Sum_Attendee_meeting_hours)] %>%
+    slice(1:top_n) %>%
+    as.data.frame()
 
   if(return == "data"){
 
@@ -81,7 +97,6 @@ meeting_extract <- function(data,
   } else if(return == "table"){
 
     mq_df %>%
-
       select(Subject,
              `Attendee hours` = "Sum_Attendee_meeting_hours",
              `Average emails per attendee hour` = "Mean_Emails_sent_per_attendee_hour",
