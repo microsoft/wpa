6a55ecd0db88438ea55f2432f0dd67c4ca704109
Martin Chan <martinchan53@gmail.com>
2021-03-09 21:24:15 +0000

    chore: align code style
    Also replaced variable name with "Cadence_of_1_on_1_meetings_with_manager" to ensure plot titles are showing up properly.

--- R/one2one_freq.R
+++ R/one2one_freq.R
@@ -6,78 +6,147 @@
 #' @title Frequency of Manager 1:1 Meetings as bar or 100% stacked bar chart
 #'
 #' @description
-#' This function calculates the average number of weeks (cadence) between of 1:1 meetings between an employee and their manager.
-#' Returns a  distribution plot for typical cadence of 1:1 meetings.
-#' Additional options available to return a bar plot, tables, or a dataframe with a Cadence of 1 on 1 meetings metric.
+#' This function calculates the average number of weeks (cadence) between of 1:1
+#' meetings between an employee and their manager. Returns a  distribution plot
+#' for typical cadence of 1:1 meetings. Additional options available to return a
+#' bar plot, tables, or a data frame with a cadence of 1 on 1 meetings metric.
 #'
-#' @param data A Standard Person Query dataset in the form of a data frame.
-#' @param hrvar HR Variable by which to split metrics, defaults to "Organization"
-#' but accepts any character vector, e.g. "LevelDesignation"
-#' @param mingroup Numeric value setting the privacy threshold / minimum group size. Defaults to 5.
-#' @param return String specifying what to return. This must be one of the following strings:
+#' @template spq-params
+#' @param return String specifying what to return. This must be one of the
+#'   following strings:
 #'   - `"plot"`
 #'   - `"table"`
-#' @param mode String specifying what method to use. This must be one of the following strings:
-#'   - `"sum"`
+#' @param mode String specifying what method to use. This must be one of the
+#'   following strings:
 #'   - `"dist"`
+#'   - `"sum"`
 #'
+#' @family Visualization
 #' @family Managerial Relations
 #'
 #' @examples
-#' # Return plot
-#' one2one_freq(sq_data, hrvar = "Organization", return = "plot")
+#' # Return plot, mode dist
+#' one2one_freq(sq_data,
+#'              hrvar = "Organization",
+#'              return = "plot",
+#'              mode = "dist")
+#'
+#' # Return plot, mode sum
+#' one2one_freq(sq_data,
+#'              hrvar = "Organization",
+#'              return = "plot",
+#'              mode = "sum")
 #'
 #' # Return summary table
 #' one2one_freq(sq_data, hrvar = "Organization", return = "table")
+#'
 #' @export
 
 one2one_freq <- function(data,
                          hrvar = "Organization",
                          mingroup = 5,
-                         return = "plot", mode="dist") {
-			
+                         return = "plot",
+                         mode="dist") {
 
+  ## Handling NULL values passed to hrvar
+  if(is.null(hrvar)){
+    data <- totals_col(data)
+    hrvar <- "Total"
+  }
 
-expanded_data <- data %>% mutate(Meetings_with_manager_1_on_1 = (Meetings_with_manager_1_on_1>0)) %>% group_by(PersonId)  %>% mutate(Cadence_of_1_on_1_meetings = 1/(sum(Meetings_with_manager_1_on_1)/n()))
+  expanded_data <-
+    data %>%
+    mutate(
+      Meetings_with_manager_1_on_1 = Meetings_with_manager_1_on_1 > 0
+      ) %>%
+    group_by(PersonId)  %>%
+    mutate(
+      Cadence_of_1_on_1_meetings_with_manager =
+        1 / (sum(Meetings_with_manager_1_on_1) / n())
+      )
 
   if(return == "data"){
-  
+
    expanded_data
-   
+
   }
-  
+
   else if(mode == "sum" & return == "plot"){
-  
-expanded_data %>% filter_all(all_vars(!is.infinite(.)))  %>% create_bar(metric="Cadence_of_1_on_1_meetings", hrvar="Organization", return="table", mingroup=mingroup)  %>% ggplot(aes(x = Cadence_of_1_on_1_meetings, y = group)) +  geom_point(colour="#FE7F4F") + scale_x_continuous("Manager 1:1 -  Frequency in Weeks") + labs(title = "Cadence of 1:1 Meetings with manager", subtitle = paste("Averages values, by ", hrvar), caption=paste("Excludes individuals with no 1:1 meetings. ", extract_date_range(data, return = "text")))  +  theme_wpa_basic() +   xlab(camel_clean(hrvar)) +
-        scale_size(guide = "none", range = c(1, 15)) +
-        theme(
-          axis.line=element_blank(),
-		  panel.grid.major.x = element_blank(),
-          panel.grid.major.y = element_line(colour = "#D9E7F7", size = 3), # lightblue bar
-		  panel.grid.minor.x = element_line(color="gray"),
-          strip.placement = "outside",
-          strip.background = element_blank(),
-          strip.text = element_blank()
-        ) 
-		
+
+    expanded_data %>%
+      filter_all(all_vars(!is.infinite(.))) %>%
+      create_bar(
+        metric = "Cadence_of_1_on_1_meetings_with_manager",
+        hrvar = hrvar,
+        return = "table",
+        mingroup = mingroup) %>%
+      ggplot(aes(x = Cadence_of_1_on_1_meetings_with_manager, y = group)) +
+      geom_point(colour = "#FE7F4F") +
+      scale_x_continuous("Manager 1:1 -  Frequency in Weeks") +
+      labs(
+        title = "Cadence of 1:1 Meetings with manager",
+        subtitle = paste("Averages values, by ", hrvar),
+        caption = paste(
+          "Excludes individuals with no 1:1 meetings. ",
+          extract_date_range(data, return = "text")
+        ),
+        x = camel_clean(hrvar)
+      )  +
+      theme_wpa_basic() +
+      scale_size(guide = "none", range = c(1, 15)) +
+      theme(
+        axis.line = element_blank(),
+        panel.grid.major.x = element_blank(),
+        panel.grid.major.y = element_line(colour = "#D9E7F7", size = 3),
+        # lightblue bar
+        panel.grid.minor.x = element_line(color = "gray"),
+        strip.placement = "outside",
+        strip.background = element_blank(),
+        strip.text = element_blank(
+        )
+        )
+
   } else if(mode == "sum" & return == "table"){
-  
-  expanded_data %>% filter_all(all_vars(!is.infinite(.)))  %>% create_bar(metric="Cadence_of_1_on_1_meetings", hrvar=hrvar, return="table", mingroup=mingroup)
+
+    expanded_data %>%
+      filter_all(all_vars(!is.infinite(.))) %>%
+      create_bar(
+        metric = "Cadence_of_1_on_1_meetings_with_manager",
+        hrvar = hrvar,
+        return = "table",
+        mingroup = mingroup
+      )
 
   } else if(mode == "dist" & return == "plot"){
 
-  expanded_data$Cadence_of_1_on_1_meetings[!is.finite(expanded_data$Cadence_of_1_on_1_meetings)] <- 99
-  expanded_data %>% create_dist(metric="Cadence_of_1_on_1_meetings", hrvar=hrvar, mingroup=mingroup, cut = c(1, 1.5, 3, 6), unit="Weeks")  
- 
- 
+    expanded_data$Cadence_of_1_on_1_meetings_with_manager[!is.finite(expanded_data$Cadence_of_1_on_1_meetings_with_manager)] <-
+      99
+
+    expanded_data %>%
+      create_dist(
+        metric = "Cadence_of_1_on_1_meetings_with_manager",
+        hrvar = hrvar,
+        mingroup = mingroup,
+        cut = c(1, 1.5, 3, 6),
+        unit = "Weeks"
+      )
+
+
+
   } else if(mode == "dist" & return == "table"){
 
-  expanded_data$Cadence_of_1_on_1_meetings[!is.finite(expanded_data$Cadence_of_1_on_1_meetings)] <- 99
-  
-  expanded_data %>% create_dist(metric="Cadence_of_1_on_1_meetings", hrvar=hrvar, mingroup=mingroup, cut = c(1, 1.5, 3, 6), unit="Weeks", return="table")  
+    expanded_data$Cadence_of_1_on_1_meetings_with_manager[!is.finite(expanded_data$Cadence_of_1_on_1_meetings_with_manager)] <-
+      99
 
-  } 
-  
- 
+    expanded_data %>%
+      create_dist(
+        metric = "Cadence_of_1_on_1_meetings_with_manager",
+        hrvar = hrvar,
+        mingroup = mingroup,
+        cut = c(1, 1.5, 3, 6),
+        unit = "Weeks",
+        return = "table"
+      )
 
+  }
 }
