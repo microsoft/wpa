f98167ce0cc42a32176678c400ac80abcf568186
Martin Chan <martinchan53@gmail.com>
2021-06-30 10:18:59 +0100

    feat: add anonymize function

---
+++ R/anonymise.R
@@ -0,0 +1,64 @@
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See LICENSE.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+
+#' @title Anonymise a categorical variable by replacing values
+#'
+#' @description
+#' Anonymize categorical variables such as HR variables by replacing values with
+#' dummy team names such as 'Team A'. The behaviour is to make 1 to 1
+#' replacements by default, but there is an option to completely randomise
+#' values in the categorical variable.
+#'
+#' @param x Character vector to be passed through.
+#' @param scramble Logical value determining whether to randomise values in the
+#' categorical variable.
+#' @param replacement Character vector containing the values to replace original
+#'   values in the categorical variable. The length of the vector must be at
+#'   least as great as the number of unique values in the original variable.
+#'   Defaults to `NULL`, where the replacement would consist of `"Team A"`,
+#'   `"Team B"`, etc.
+#'
+#' @examples
+#' unique(anonymise(sq_data$Organization))
+#'
+#' rep <- c("Manager+", "Manager", "IC")
+#' unique(anonymise(sq_data$Layer), replacement = rep)
+#'
+#' @export
+
+anonymise <- function(x,
+                      scramble = FALSE,
+                      replacement = NULL){
+
+  n_to_rep <- length(x)
+  v_to_rep <- unique(x)
+  nd_to_rep <- length(v_to_rep)
+
+  if(is.null(replacement)){
+
+    replacement <- paste("Team", LETTERS[1:nd_to_rep])
+
+  } else {
+
+    replacement <- replacement[1:nd_to_rep]
+
+  }
+
+  if(scramble == TRUE){
+
+    sample(x = replacement,
+           size = n_to_rep,
+           replace = TRUE)
+
+  } else if(scramble == FALSE){
+
+    replacement[match(x, v_to_rep)]
+
+  }
+}
+
+#' @rdname anonymise
+#' @export
+anonymize <- anonymise
