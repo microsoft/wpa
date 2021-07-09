9a4cd2d176229554f19f7881db471c6af266b465
Martin Chan <martinchan53@gmail.com>
2021-07-09 11:55:12 +0100

    feat: add subject_scan (#172)

---
+++ R/subject_scan.R
@@ -0,0 +1,53 @@
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See LICENSE.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+
+#' @title
+#' Count top words in subject lines grouped by a custom attribute
+#'
+#' @description `r lifecycle::badge('experimental')`
+#'
+#' This function generates a matrix of the top occurring words in meetings,
+#' grouped by a specified attribute such as organisational attribute, day of the
+#' week, or hours of the day.
+#'
+#' @param data A Meeting Query dataset in the form of a data frame.
+#' @param hrvar String containing the name of the HR Variable by which to split
+#'   metrics.
+#' @param n Numeric value specifying the top number of words to show.
+#'
+#' @examples
+#' mt_data %>% subject_scan()
+#'
+#' @export
+subject_scan <- function(data, hrvar, n = 10){
+
+  out_tb <-
+    test_mt %>%
+    group_split(!!sym(hrvar)) %>%
+    purrr::map(function(x){
+
+      dow <- x[[hrvar]][1]
+
+      long_t <- tm_clean(x)
+
+      long_t %>%
+        count(word) %>%
+        arrange(desc(n)) %>%
+        head(n) %>%
+        rename(
+          !!sym(paste0(dow, "_word")) := "word",
+          !!sym(paste0(dow, "_n")) := "n"
+        )
+    }) %>%
+    bind_cols() %>%
+
+  out_simple <-
+    out_tb %>%
+    select(-ends_with("_n")) %>%
+    set_names(nm = gsub(pattern = "_word", replacement = "",
+                        x = names(.)))
+
+  return(out_simple)
+}
