ff9713b4ee5f60defa918429e773ab0570d62ffa
Martin Chan <martinchan53@gmail.com>
2021-11-09 11:28:16 +0000

    feat: add bar chart option
    - Remove option for `show_n`
    - Added bar chart option
    - Made plotting functions modular (two unexported functions)

--- R/create_inc.R
+++ R/create_inc.R
@@ -1,26 +1,26 @@
 #' @title
-#' Create an incidence table reflecting proportion of population scoring above
+#' Create an incidence analysis reflecting proportion of population scoring above
 #' or below a threshold for a metric
 #'
 #' @description
-#' An incidence table is generated, with each value in the table reflecting the
-#' proportion of the population that is above or below a threshold for a
-#' specified metric. Two values are required for `hrvar` in order to generate
-#' this incidence table.
+#' An incidence analysis is generated, with each value in the table reflecting
+#' the proportion of the population that is above or below a threshold for a
+#' specified metric. There is an option to only provide a single `hrvar` in
+#' which a bar plot is generated, or two `hrvar` values where an incidence table
+#' (heatmap) is generated.
+#'
 #'
 #' @param data A Standard Person Query dataset in the form of a data frame.
 #' @param metric Character string containing the name of the metric,
 #' e.g. "Collaboration_hours"
-#' @param hrvar Character vector of length 2 containing the name of the HR
-#'   Variable by which to split metrics.
+#' @param hrvar Character vector of at most length 2 containing the name of the
+#'   HR Variable by which to split metrics.
 #' @param mingroup Numeric value setting the privacy threshold / minimum group
 #'   size. Defaults to 5.
 #' @param threshold Numeric value specifying the threshold.
 #' @param position String containing the below valid values:
 #'   - `"above"`: show incidence of those equal to or above the threshold
 #'   - `"below"`: show incidence of those equal to or below the threshold
-#' @param show_n Logical. Toggle to set whether text labels in plot to show the
-#' number of employees (`n`). Defaults to `TRUE`.
 #' @param return String specifying what to return. This must be one of the
 #'   following strings:
 #'   - `"plot"`
@@ -41,6 +41,16 @@
 #' @family Flexible
 #'
 #' @examples
+#' # Only a single HR attribute
+#' create_inc(
+#'   data = sq_data,
+#'   metric = "After_hours_collaboration_hours",
+#'   hrvar = "Organization",
+#'   threshold = 4,
+#'   position = "above"
+#' )
+#'
+#' # Two HR attributes
 #' create_inc(
 #'   data = sq_data,
 #'   metric = "Collaboration_hours",
@@ -58,23 +68,123 @@ create_inc <- function(
   mingroup = 5,
   threshold,
   position,
-  show_n = TRUE,
   return = "plot"
 ){
 
-  if(length(hrvar) != 2){
+  if(length(hrvar) == 1){
+
+    create_inc_bar(
+      data = data,
+      metric = metric,
+      hrvar = hrvar,
+      mingroup = mingroup,
+      threshold = threshold,
+      position = position,
+      return = return
+    )
+
+  } else if(length(hrvar) == 2){
+
+    create_inc_grid(
+      data = data,
+      metric = metric,
+      hrvar = hrvar,
+      mingroup = mingroup,
+      threshold = threshold,
+      position = position,
+      return = return
+    )
+
+
+  } else {
+
     stop("`hrvar` can only accept a character vector of length 2.")
+
   }
 
+}
 
+#' @rdname create_inc
+#' @export
+create_incidence <- create_inc
+
+
+#' Run `create_inc` with only single `hrvar`
+#' Returning a bar chart
+#'
+#' @noRd
+create_inc_bar <- function(
+  data,
+  metric,
+  hrvar,
+  mingroup = 5,
+  threshold,
+  position,
+  return = "plot"
+){
+
+  # Transform data so that metrics become proportions
+  data_t <-
+    data %>%
+    { if (position == "above"){
+      mutate(., !!sym(metric) := !!sym(metric) >= threshold)
+    } else if (position == "below"){
+      mutate(., !!sym(metric) := !!sym(metric) <= threshold)
+    }
+    }
+
+  # Set title text
+  title_text <-
+    paste(
+      "Incidence of",
+      tolower(us_to_space(metric)),
+      position,
+      threshold
+    )
+
+  # Set subtitle text
+  subtitle_text <-
+    paste(
+      "Percentage and number of employees by",
+      hrvar
+    )
+
+  # Pipe result to `create_bar()`
+  create_bar(
+    data = data_t,
+    metric = metric,
+    hrvar = hrvar,
+    mingroup = mingroup,
+    return = return,
+    plot_title = title_text,
+    plot_subtitle = subtitle_text,
+    percent = TRUE
+  )
+}
+
+#' Run `create_inc` with only two `hrvar`
+#' Returning a heatmap
+#'
+#' @noRd
+create_inc_grid <- function(
+  data,
+  metric,
+  hrvar,
+  mingroup = 5,
+  threshold,
+  position,
+  return = "plot"
+){
+
+  # Create table of proportions
   myTable <-
     data %>%
     { if (position == "above"){
-        mutate(., !!sym(metric) := !!sym(metric) >= threshold)
-      } else if (position == "below"){
-        mutate(., !!sym(metric) := !!sym(metric) <= threshold)
-      }
-      } %>%
+      mutate(., !!sym(metric) := !!sym(metric) >= threshold)
+    } else if (position == "below"){
+      mutate(., !!sym(metric) := !!sym(metric) <= threshold)
+    }
+    } %>%
     group_by(!!sym(hrvar[1]), !!sym(hrvar[2]), PersonId) %>%
     summarise(
       !!sym(metric) := mean(!!sym(metric), na.rm = TRUE)
@@ -94,7 +204,6 @@ create_inc <- function(
 
   } else if(return == "plot"){
 
-
     # Set title text
     title_text <-
       paste(
@@ -105,57 +214,25 @@ create_inc <- function(
       )
 
     # Set subtitle text
-    if(show_n == TRUE){
-
-      subtitle_text <-
-        paste(
-          "Percentage and number of employees by",
-          hrvar[1],
-          "and",
-          hrvar[2]
-        )
-
-    } else if(show_n == FALSE){
-
-      subtitle_text <-
-        paste(
-          "Percentage of employees by",
-          hrvar[1],
-          "and",
-          hrvar[2]
-        )
-
-    }
-
-
+    subtitle_text <-
+      paste(
+        "Percentage and number of employees by",
+        hrvar[1],
+        "and",
+        hrvar[2]
+      )
 
     myTable %>%
-      {
-        if (show_n == TRUE){
-          mutate(., metric_text = paste0(
-            scales::percent(!!sym(metric), accuracy = 1),
-            " (", n, ")"))
-        } else {
-          .
-        }
-      } %>%
+      mutate(metric_text = paste0(
+        scales::percent(!!sym(metric), accuracy = 1),
+        " (", n, ")")) %>%
       ggplot(aes(x = !!sym(hrvar[1]),
                  y = !!sym(hrvar[2]),
                  fill = !!sym(metric))) +
       geom_tile() +
-      {
-        if (show_n == TRUE){
-          geom_text(
-            aes(label = metric_text),
-            colour = "black",
-            size = 3)
-        } else {
-
-          geom_text(aes(label = scales::percent(!!sym(metric), accuracy = 1)),
-                    colour = "black",
-                    size = 3)
-        }
-      } +
+      geom_text(aes(label = metric_text),
+                colour = "black",
+                size = 3)+
       scale_fill_gradient2(low = rgb2hex(7, 111, 161),
                            mid = rgb2hex(241, 204, 158),
                            high = rgb2hex(216, 24, 42),
@@ -175,16 +252,5 @@ create_inc <- function(
           "Percentages reflect incidence with respect to population in cell."),
         fill = "Incidence"
       )
-
-  } else {
-
-    stop("please enter a valid value for `return`.")
-
   }
 }
-
-#' @rdname create_inc
-#' @export
-create_incidence <- create_inc
-
-
