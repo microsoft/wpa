78f75d6daf5a2ec850482bcb69c8031bb801c82e
C Morales <62895857+moralec@users.noreply.github.com>
2022-05-30 14:46:34 +0100

    New naming convention

--- R/external_sum.R
+++ R/external_sum.R
@@ -3,10 +3,10 @@
 # Licensed under the MIT License. See LICENSE.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
 
-#' @title Collaboration -  External / Internal
+#' @title External Collaboration Summary
 #'
 #' @description
-#' Provides a split of 'Weekly Digital Collaboration' between internal and external accounts.
+#' Provides an overview analysis of 'External Collaboration'.
 #' Returns a stacked bar plot of internal and external collaboration.
 #' Additional options available to return a summary table.
 #'
@@ -18,23 +18,28 @@
 #'
 #' @examples
 #' # Return a  plot
-#' collaboration_external(sq_data, hrvar = "LevelDesignation")
+#' external_sum(sq_data, hrvar = "LevelDesignation")
 #'
 #' # Return summary table
-#' collaboration_external(sq_data, hrvar = "LevelDesignation", return = "table")
+#' external_sum(sq_data, hrvar = "LevelDesignation", return = "table")
 #'
 #' @export
 
-collaboration_external <- function(data,
+external_sum <- function(data,
                                 hrvar = "Organization",
                                 mingroup = 5,
+								stack_colours = c("#1d327e", "#1d7e6a"),
                                 return = "plot"){
 
 
 # Calculate Internal / External Collaboration time 
-plot_data <-  data %>% mutate(Internal_collaboration_hours =  Collaboration_hours - Collaboration_hours_external)   %>%  mutate(External_collaboration_hours =  Collaboration_hours_external) 
+plot_data <-  data %>% mutate(Internal_hours=  Collaboration_hours - Collaboration_hours_external)   %>%  mutate(External_hours=  Collaboration_hours_external) 
   
 # Plot Internal / External Collaboration time by Organization
-plot_data %>%  create_stacked(hrvar = hrvar, metrics = c("Internal_collaboration_hours", "External_collaboration_hours"),   plot_title = "Collaboration Hours - Internal / External",  stack_colours = c("#1d7e6a","#1d327e"), mingroup = mingroup, return = return)
+plot_data %>%  create_stacked(hrvar = hrvar, metrics = c("Internal_hours", "External_hours"),   plot_title = "Internal and External Collaboration Hours",  stack_colours = stack_colours, mingroup = mingroup, return = return)
 
 }
+
+#' @rdname external_sum
+#' @export
+external_summary <- external_sum
