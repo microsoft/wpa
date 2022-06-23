dcff4ed1df591ea591ea4d7405ccfbd968ad1f39
Martin Chan <martinchan53@gmail.com>
2022-06-23 16:54:34 +0100

    tests: cover workpatterns_classify

--- tests/testthat/test-workpatterns_classify.R
+++ tests/testthat/test-workpatterns_classify.R
@@ -1,10 +1,33 @@
 test_that(
-  desc = "Test workpatterns outputs",
+  desc = "workpatterns data output row matches - signals",
   code = {
     out <- workpatterns_classify(em_data, signals = "IM", return = "data")
     expect_equal(
       object = nrow(out),
       expected = nrow(em_data)
     )
+
+    out <- workpatterns_classify(em_data, signals = "unscheduled_calls", return = "data")
+    expect_equal(
+      object = nrow(out),
+      expected = nrow(em_data)
+    )
+
+    out <- workpatterns_classify(em_data, signals = "meetings", return = "data")
+    expect_equal(
+      object = nrow(out),
+      expected = nrow(em_data)
+    )
   }
 )
+
+test_that("workpatterns plots returns ggplot object",{
+  p <- workpatterns_classify(em_data, signals = "meetings", return = "plot")
+  expect_s3_class(p, "ggplot")
+
+  p <- workpatterns_classify(em_data, signals = "IM", return = "plot")
+  expect_s3_class(p, "ggplot")
+
+  p <- workpatterns_classify(em_data, signals = "unscheduled_calls", return = "plot")
+  expect_s3_class(p, "ggplot")
+})
